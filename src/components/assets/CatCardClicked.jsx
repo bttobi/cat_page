64b3b0ab82018bf94546021957d7334a7fb87755
bttobi <bttobi123@gmail.com>
2023-03-05 20:11:58 +0100

    added depth

--- src/components/assets/CatCardClicked.jsx
+++ src/components/assets/CatCardClicked.jsx
@@ -4,7 +4,7 @@ const CatCardClicked = (props) => {
   const [isClosed, setIsClosed] = useState(true);
 
   return (
-    <div className={isClosed + " cat-details static top-20 left-auto m-0 w-min h-min fixed flex self-start justify-self-start flex-col border-8 border-secondary-white rounded-lg transition-all duration-300 opacity-100"}>
+    <div className={isClosed + " cat-details z-11 static top-20 left-auto m-0 w-min h-min fixed flex self-start justify-self-start flex-col border-8 border-secondary-white rounded-lg transition-all duration-300 opacity-100"}>
       <button className="close-button absolute right-0 m-1 transition-all duration-150 hover:scale-110" onClick={(e, isClosed) => {props.func(e, true);}}><img src="/img/close.png" height="32px" width="32px" alt="Home"/></button>
       <div className="cat-description-wrapper bg-black">
         {(props.cat.breeds[0]!=undefined || props.cat.breeds[0]!=null) ?
