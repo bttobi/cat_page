55c2db4b5baf21f3f42dad53295a37a2310cbdab
bttobi <bttobi123@gmail.com>
2023-03-11 17:09:55 +0100

    updated look of clicked card

--- src/components/assets/CatCardClicked.jsx
+++ src/components/assets/CatCardClicked.jsx
@@ -1,11 +1,11 @@
 const CatCardClicked = (props) => {
   return (
-    <div className={"cat-details z-20 mt-20 left-auto m-0 w-min h-min fixed flex self-start justify-self-start flex-col border-8 border-secondary-white rounded-lg transition-all duration-300 opacity-100"}>
+    <div className={"cat-details z-20 mt-20 left-auto m-0 w-min h-min fixed flex self-start justify-self-start flex-col border-4 border-secondary-white rounded-lg transition-all duration-300 opacity-100"}>
       <button className="close-button absolute right-0 m-1 transition-all duration-150 hover:scale-110" onClick={(e, isShown) => {props.func(e, true);}}><img src="/img/close.png" height="32px" width="32px" alt="Home"/></button>
       <div className="cat-description-wrapper bg-black">
         {(props.cat.breeds[0]!=undefined || props.cat.breeds[0]!=null) ?
-        <div className="cat-name-details font-article text-center pt-8 pb-8 bg-primary border-b-8 border-secondary-white text-4xl font-bold">{props.cat.breeds[0].name}</div>
-        : <div className="cat-name-details font-article text-center pt-8 pb-8 bg-primary border-b-8 border-secondary-white text-4xl font-bold">{"Cute Cat"}</div>
+        <div className="cat-name-details font-article text-center pt-8 pb-8 bg-primary border-secondary-white text-4xl font-bold">{props.cat.breeds[0].name}</div>
+        : <div className="cat-name-details font-article text-center pt-8 pb-8 bg-primary border-secondary-white text-4xl font-bold">{"Cute Cat"}</div>
         }
         <div className="cat-description flex flex-col p-2 bg-primary">
         <div className="cat-img-details w-96 h-72 pt-0"><div className="cat-image w-96 h-72 rounded-lg" style={{backgroundImage: `url(${props.cat.url})`,  backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}></div></div>
--- src/components/pages/Random.jsx
+++ src/components/pages/Random.jsx
@@ -9,7 +9,7 @@ const Random = () => {
   const [showClicked, setShowClicked] = useState(false);
 
   const fetchData = async () => {
-    const URL = `https://api.thecatapi.com/v1/images/search?limit=8&api_key=${process.env.REACT_APP_API_KEY}`;
+    const URL = `https://api.thecatapi.com/v1/images/search?limit=15&api_key=${process.env.REACT_APP_API_KEY}`;
       const response = await fetch(URL);
       return response.json();
   }
@@ -29,7 +29,7 @@ const Random = () => {
   useEffect(()=>{
     const onScroll = async (e)=>{
       const { scrollHeight, scrollTop, clientHeight } = e.target.scrollingElement;
-      if(!query.isFetching && scrollHeight - scrollTop <= clientHeight * 1.75){
+      if(!query.isFetching && scrollHeight - scrollTop <= clientHeight *1.25){
         let newCat = await query.refetch();
         setDataToDisplay([...dataToDisplay, newCat.data[0], newCat.data[1], newCat.data[2], newCat.data[3]]);
       }
@@ -57,7 +57,7 @@ const Random = () => {
   return (
     <div id="home" className="home-page w-full h-full m-none mt-16  flex flex-col justify-center items-center font-article text-white">
       <AnimatePresence>
-        {showClicked && <motion.div initial={{ opacity: 0}} animate={{opacity: 0.7}} exit={{opacity: 0}} className="backdrop w-full h-full fixed flex flex-col bg-black z-10 align-center justify-center items-center"></motion.div>}
+        {showClicked && <motion.div initial={{ opacity: 0 }} animate={{opacity: 0.7}} exit={{opacity: 0}} className="backdrop w-full h-full fixed flex flex-col bg-black z-10 align-center justify-center items-center"></motion.div>}
       </AnimatePresence>
       <p className="scroll-desc mt-16"> SCROLL DOWN TO LOAD CATS!</p>
       <div className="cat-cards-wrapper w-full h-full flex flex-row flex-wrap items-center justify-center">
