104e6238957da89531e29d2e7cf5c9d4728e1f78
bttobi <bttobi123@gmail.com>
2023-03-11 18:33:33 +0100

    better button styling on clicked card

--- src/components/assets/CatCardClicked.jsx
+++ src/components/assets/CatCardClicked.jsx
@@ -2,7 +2,7 @@ const CatCardClicked = (props) => {
   return (
     <div className={"cat-details z-20 left-auto m-0 w-min h-min fixed flex flex-grow flex-col border-4 border-secondary-white rounded-lg transition-all duration-300 opacity-100"}>
       <div className="cat-description-wrapper w-full h-full p-2 bg-primary">
-        <button className="close-button btn btn-sm btn-square bg-primary right-0 mr-2 btn-outline absolute m-1 transition-all duration-150 hover:scale-110" onClick={(e, isShown) => {props.func(e, true);}}>
+        <button className="close-button btn btn-sm btn-error btn-square bg-primary right-0 mr-2 btn-outline absolute m-1 transition-all duration-150 hover:scale-110" onClick={(e, isShown) => {props.func(e, true);}}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
         </button>
         {(props.cat.breeds[0]!=undefined || props.cat.breeds[0]!=null) ?
