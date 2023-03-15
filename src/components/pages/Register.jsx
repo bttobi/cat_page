dbbaa5582190bc23fb9caf0a3740ec91e9d90aa8
bttobi <bttobi123@gmail.com>
2023-03-15 17:15:36 +0100

    small changes

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -8,8 +8,7 @@ const Register = () => {
 
   const register = async () => {
     try {
-      const user = await createUserWithEmailAndPassword(auth, registerEmail, password);
-      console.log(user);
+      await createUserWithEmailAndPassword(auth, registerEmail, password);
     }
     catch(error){
       console.error(error); 
@@ -29,7 +28,7 @@ const Register = () => {
 
   return (
     <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="flex flex-col justify-center align-center items-center font-article text-white w-32 h-min mt-40">
-      <form className="flex flex-col align-center items-center justify-center text-center" action="" method="post">
+      <div className="flex flex-col align-center items-center justify-center text-center" action="" method="post">
         <div className="username flex flex-col align-center justify-center w-max">
           <label htmlFor="email">Email</label>
           <input ref={emailRef} type="email" name="email" placeholder="Email" required onChange={()=>{setRegisterEmail(emailRef.current.value)}}/>
@@ -43,7 +42,7 @@ const Register = () => {
           <input ref={passConfRef} type="password" name="confirm-password" placeholder="Confirm Password" required onChange={()=>{setConfirmPassword(passConfRef.current.value)}}/>
         </div>
         <button className="login-button btn btn-sm bg-primary text-article text-secondary-white text-sm border-2 border-secondary-white rounded-md transition-all duration-300 hover:border-secondary-white hover:bg-bg-primary mt-4" onClick={register}>Register</button>
-      </form>
+      </div>
       <div className="login-redirect text-center mt-10">Already have an account? Create one <button className="login-redirect-button underline text-center"><Link to="/login">here</Link></button></div>
     </motion.div>
   )
