0962f5ce98c5f64f97708a2e97beea024628229e
bttobi <bttobi123@gmail.com>
2023-03-17 23:50:36 +0100

    added sign out option

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 import {motion} from 'framer-motion';
 import { auth } from '../../firebase.js';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 
 const Profile = () => {
+  const [user, setUser] = useState({});
+  
+  onAuthStateChanged(auth, (currentUser) =>{
+    setUser(currentUser);
+  });
+
+  const logOut = async () => {
+    await signOut(auth);
+  };
+
   return (
     <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="font-article text-white w-full h-full mt-16">
-      {auth.currentUser?.email}
+      {user.email}<br></br>
+      <button onClick={logOut}>Sign out</button>
     </motion.div>
   )
 }
