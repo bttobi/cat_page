cd3822e9d2cd76964bb8ed7ceb9150f5099b5020
bttobi <bttobi123@gmail.com>
2023-03-20 23:57:57 +0100

    added user context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { createContext } from 'react';
+import { auth } from './firebase.js';
 import { motion, AnimatePresence } from 'framer-motion';
 import Navbar from './components/navigation/Navbar';
 import { Route, Routes, BrowserRouter } from "react-router-dom";
@@ -10,28 +12,32 @@ import Register from './components/pages/Register';
 import Profile from './components/pages/Profile';
 import Random from './components/pages/Random';
 
+export const UserContext = createContext();
+
 function App() {
   const queryClient = new QueryClient();
-  
+
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-          <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="App w-full h-full flex flex-col items-center relative">
-            <Navbar />
-            <AnimatePresence>
-              <Routes>
-                <Route path='/' element={<Random/>}/>
-                <Route path='cat_breeds' element={<CatBreeds/>}/>
-                <Route path='favourites' element={<Favourites/>}/>
-                <Route path='about' element={<About/>}/>
-                <Route path='login' element={<Login/>}/>
-                <Route path='register' element={<Register/>}/>
-                <Route path='profile' element={<Profile/>}/>
-              </Routes>
-            </AnimatePresence>
-          </motion.div>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <UserContext.Provider value={auth}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="App w-full h-full flex flex-col items-center relative">
+              <Navbar />
+              <AnimatePresence>
+                <Routes>
+                  <Route path='/' element={<Random/>}/>
+                  <Route path='cat_breeds' element={<CatBreeds/>}/>
+                  <Route path='favourites' element={<Favourites/>}/>
+                  <Route path='about' element={<About/>}/>
+                  <Route path='login' element={<Login/>}/>
+                  <Route path='register' element={<Register/>}/>
+                  <Route path='profile' element={<Profile/>}/>
+                </Routes>
+              </AnimatePresence>
+            </motion.div>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
