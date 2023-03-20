cd322526629c6d8d557d73b1ae87fd96bf8ed2f3
bttobi <bttobi123@gmail.com>
2023-03-20 23:58:39 +0100

    tweaks with routing and styling

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,12 +1,11 @@
 import { NavLink } from 'react-router-dom';
-import { auth } from '../../firebase.js';
 import { onAuthStateChanged } from 'firebase/auth';
-import { useState } from 'react';
-
-const buttonStyle= "btn bg-primary text-secondary-white text-lg border-0 p-3 rounded-md transition-all duration-300 hover:bg-bg-primary hover:bg-dark";
+import { useState, useContext } from 'react';
+import { UserContext } from '../../App';
 
 const Navbar = () => {
   const [user, setUser] = useState({});
+  const auth = useContext(UserContext);
   
   onAuthStateChanged(auth, (currentUser) =>{
     setUser(currentUser);
@@ -17,18 +16,18 @@ const Navbar = () => {
       <ul className="w-full h-full flex flex-row justify-between items-center">
         <li className="flex flex-row p-3 rounded-md transition-all duration-300 hover:rotate-12">
           <NavLink to="/" className="flex flex-row justify-center items-center">
-            <img src="/img/paw.png" height="40px" width="40px" alt="random"/>
+            <img className="bg-primary" src="/img/paw.png" height="40px" width="40px" alt="home/random page icon with link"/>
           </NavLink>
         </li>
         <li className="w-full flex justify-center align-center">
-          <button className={buttonStyle + " mx-1"}><NavLink to="/">Random Cats</NavLink></button>
-          <button className={buttonStyle + " mx-1"}><NavLink to="/cat_breeds">Cat Breeds</NavLink></button>
-          <button className={buttonStyle + " mx-1"}><NavLink to="/favourites">Favourites</NavLink></button>
-          <button className={buttonStyle + " mx-1"}><NavLink to="/about">About</NavLink></button>
+          <NavLink to="/" className="home-link bg-primary p-2 transition-all duration-250 hover:bg-dark rounded-lg text-white outline-none mx-1">Random Cats</NavLink>
+          <NavLink to="/cat_breeds" className="cat-breeds-link bg-primary p-2 transition-all duration-250 hover:bg-dark rounded-lg text-white outline-none mx-1">Cat Breeds</NavLink>
+          <NavLink to="/favourites" className="favourites-link bg-primary p-2 transition-all duration-250 hover:bg-dark rounded-lg text-white outline-none mx-1">Favourites</NavLink>
+          <NavLink to="/about" className="about-link bg-primary p-2 transition-all duration-250 hover:bg-dark rounded-lg text-white outline-none mx-1">About</NavLink>
         </li>
         {user?.email ?  
-        <li className={buttonStyle + " flex justify-center items-center"} style={{padding: "3px", right:"0", marginRight: "6px"}}><NavLink to="/profile"><img src="/img/cat_profile.png" height="40px" width="40px" alt="profile"/></NavLink></li> 
-        : <li className={buttonStyle + " flex justify-center items-center"} style={{width:"52px", height:"52px", right:"0", marginRight: "6px"}}><NavLink to="/login"><i className="gg-log-in"/></NavLink></li>
+        <NavLink to="/profile" className="profile-link bg-primary transition-all duration-250 hover:bg-dark rounded-lg text-white outline-none"><li className={" flex justify-center items-center"} style={{padding: "3px"}}><img src="/img/cat_profile.png" height="40px" width="40px" alt="profile link/image"/></li></NavLink> 
+        : <NavLink to="/login" className="login-link bg-primary transition-all duration-250 hover:bg-dark rounded-lg text-white outline-none"><li className={" flex justify-center items-center"} style={{width:"40px", height:"46px", marginRight: "6px"}}><i className="gg-log-in" alt="login picture"/></li></NavLink>
         }
       </ul>
     </nav>
