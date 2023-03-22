de742f5e7f554c5419f32b332c99eebc02c94fcb
bttobi <bttobi123@gmail.com>
2023-03-22 22:46:23 +0100

    fixed adding incorrect info to firebase

--- src/components/assets/CatCard.jsx
+++ src/components/assets/CatCard.jsx
@@ -28,16 +28,16 @@ const CatCard = (props) => {
     }
     
     try{
-      const hasBreed = (props.cat.breeds[0]!==undefined ||  props.cat.breeds[0]!==null) ? props.cat.breeds[0] : "";
+      const hasBreed = (props.cat.breeds[0]!==undefined ||  props.cat.breeds[0]!==null) ? false : true;
       const newCat = 
       {
-        breed: "kotek",
+        breed_name: hasBreed ? props.cat.breeds[0].name : "Cute Cat",
         image: props.cat.url,
-        origin: "test",
-        temperament: "test",
-        weight: "test",
-        life_span: "test",
-        wiki_link: "test"
+        origin: hasBreed ? props.cat.breeds[0].origin : "",
+        temperament: hasBreed ? props.cat.breeds[0].temperament : "",
+        weight: hasBreed ? props.cat.breeds[0].weight.metric : "",
+        life_span: hasBreed ? props.cat.breeds[0].life_span : "",
+        wikipedia_url: hasBreed ? props.cat.breeds[0].wikipedia_url : ""
       };
 
       const collectionRef = collection(db, auth.currentUser.email);
