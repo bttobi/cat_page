da654f01dbacdd8d86f719af325d35c1763d0970
bttobi <bttobi123@gmail.com>
2023-03-22 23:42:14 +0100

    code shortening

--- src/components/assets/CatCardClicked.jsx
+++ src/components/assets/CatCardClicked.jsx
@@ -5,10 +5,7 @@ const CatCardClicked = (props) => {
         <button className="close-button btn btn-sm btn-error btn-square bg-primary right-0 mr-2 btn-outline absolute m-1 transition-all duration-150 hover:scale-110" onClick={(e, isShown) => {props.func(e, true);}}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
         </button>
-        {(props.cat.breeds[0]!=undefined || props.cat.breeds[0]!=null) ?
-        <div className="cat-name-details font-article text-center pt-6 pb-8 bg-primary border-secondary-white text-4xl font-bold">{props.cat.breeds[0].name}</div>
-        : <div className="cat-name-details font-article text-center pt-6 pb-8 bg-primary border-secondary-white text-4xl font-bold">{"Cute Cat"}</div>
-        }
+        <div className="cat-name-details font-article text-center pt-6 pb-8 bg-primary border-secondary-white text-4xl font-bold">{props.cat.breed_name || "Cute Cat"}</div>
         <div className="cat-image rounded-lg" style={{width: '24rem', height: '20rem', backgroundImage: `url(${props.cat.url})`,  backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}></div>
       </div>
     </div>
