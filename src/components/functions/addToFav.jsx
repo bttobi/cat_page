6998cc5bd5f48815e2a3ab9cc80e5d49f61753cc
bttobi <bttobi123@gmail.com>
2023-03-24 00:58:55 +0100

    added new function addToFav

---
+++ src/components/functions/addToFav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { collection, doc, setDoc } from 'firebase/firestore';
+import db from '../../firebase';
+
+const addTofav = async (props, auth) => {
+      const hasBreed = (props.cat.breeds[0]!==undefined ||  props.cat.breeds[0]!==null) ? false : true;
+      const newCat = 
+      {
+        breed_name: hasBreed ? props.cat.breeds[0].name : "Cute Cat",
+        url: props.cat.url,
+        origin: hasBreed ? props.cat.breeds[0].origin : "",
+        temperament: hasBreed ? props.cat.breeds[0].temperament : "",
+        weight: hasBreed ? props.cat.breeds[0].weight.metric : "",
+        life_span: hasBreed ? props.cat.breeds[0].life_span : "",
+        wikipedia_url: hasBreed ? props.cat.breeds[0].wikipedia_url : ""
+      };
+
+      const collectionRef = collection(db, auth.currentUser.email);
+      const documentRef = doc(collectionRef, newCat.url.replace('https://cdn2.thecatapi.com/images/', ''));
+      await setDoc(documentRef, newCat);
+}
+
+export default addTofav
