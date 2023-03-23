8fc2f41f61b50db6f88a45138e98ce99a5350cc5
bttobi <bttobi123@gmail.com>
2023-03-24 00:23:35 +0100

    created hook for getting favourite cats

---
+++ src/components/hooks/useFavourites.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useContext } from 'react';
+import db from '../../firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import { UserContext } from '../../App';
+
+const useFavourites = () => {
+  const auth = useContext(UserContext);
+  const [cats, setCats] = useState([]);
+
+  const getFavCats = async () => {
+    if(auth.currentUser?.email===null || auth.currentUser?.email===undefined){
+      return;
+    }
+    
+    try{
+      const collectionRef =  collection(db, auth.currentUser.email);
+      const listOfCats = await getDocs(collectionRef);
+
+      const toAdd = [];
+      listOfCats.forEach(favCat => toAdd.push(favCat.data()));
+      setCats(toAdd);
+    }
+
+    catch(error){
+      console.log(error)
+    }
+  }
+  
+  useEffect(()=>{
+    getFavCats();
+  },[]);
+
+  return cats;
+}
+
+export default useFavourites
