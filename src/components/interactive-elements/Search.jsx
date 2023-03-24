f0015e21da53bb298d19a12f4e84305f6e4f4975
bttobi <bttobi123@gmail.com>
2023-03-25 00:53:52 +0100

    deleted number of cats in search
    now only needing a cat breed

--- src/components/interactive-elements/Search.jsx
+++ src/components/interactive-elements/Search.jsx
@@ -3,17 +3,15 @@ import { useState, useRef, useEffect } from 'react';
 import { useQuery } from 'react-query';
 
 const Search = (props) => {
-  const inputBreed = useRef("abys");
-  const inputNumber = useRef(1);
+  const inputBreed = useRef("beng");
 
   const [searchDetails, setSearchDetails] = useState(
   {
-    number: 1,
-    breed: "abys"
+    breed: "beng"
   });
 
-  const updateSearchDetails = (number, breed) => {
-    const nextSearchDetails = {...searchDetails, number, breed}
+  const updateSearchDetails = (breed) => {
+    const nextSearchDetails = {...searchDetails, breed}
     setSearchDetails(nextSearchDetails);
   }
 
@@ -37,12 +35,10 @@ const Search = (props) => {
 
   return (
     <div className="search-wrapper text-center w-1/2 flex flex-col flex-nowrap items-center">
-      <label className="mt-4 mb-4 font-article" htmlFor="cat-breed">How many cats do you want to generate?</label>
-      <input className="w-48 bg-bg-primary border-2 border-secondary-white rounded-lg text-center" type="number" min="1" max="12" onChange={() => {updateSearchDetails(inputNumber.current.value, inputBreed.current.value)}} ref={inputNumber} value={searchDetails.number} name="cat-number"/>
       <div className="search-settings flex flex-row flex-wrap justify-center items-center">
         <label className="mt-10 mb-4 font-article" htmlFor="cat-breed">What cat breed do you want to see?</label>
       </div>
-      <select className=" w-48 bg-bg-primary border-2 border-secondary-white rounded-lg text-center" onChange={() => {updateSearchDetails(inputNumber.current.value, inputBreed.current.value)}} ref={inputBreed} value={searchDetails.breed} name="cat-breed">
+      <select className=" w-48 bg-bg-primary border-2 border-secondary-white rounded-lg text-center" onChange={() => {updateSearchDetails(inputBreed.current.value)}} ref={inputBreed} value={searchDetails.breed} name="cat-breed">
       {breeds.isFetching ?  <option value="">{"Select"}</option> : 
         breeds.data.map((el, i) => {
         return <option key={i} value={el.id} className="w-6 h-6">{el.name}</option>
