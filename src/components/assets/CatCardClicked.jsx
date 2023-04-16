dece5cd1cb4c8b520b9f0f4de205eb1df1ab1d1a
bttobi <bttobi123@gmail.com>
2023-04-16 17:39:27 +0200

    more styling fixes

--- src/components/assets/CatCardClicked.jsx
+++ src/components/assets/CatCardClicked.jsx
@@ -1,7 +1,7 @@
 const CatCardClicked = (props) => {
   return (
-    <div className="cat-details z-20 m-0 fixed top-0 flex flex-col rounded-lg transition-all duration-300 opacity-100" style={{minWidth:"40vw", maxWidth: "70vw"}}>
-      <div className="flex flex-col flex-grow cat-description-wrapper p-2 bg-primary rounded-lg shadow-lg shadow-black"  style={{background: "rgba(0, 0, 0, .65) " + `url(${props.cat.url})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundBlendMode: "darken"}}>
+    <div className="cat-details z-20 m-0 fixed top-0 flex flex-col rounded-lg transition-all duration-300 opacity-100" style={{minWidth:"90vw", maxWidth: "90vw"}}>
+      <div className="flex flex-col flex-grow cat-description-wrapper p-2 bg-primary rounded-lg shadow-lg shadow-black justify-center align-center items-center"  style={{minHeight: "80vh", background: "rgba(0, 0, 0, .65) " + `url(${props.cat.url})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundBlendMode: "darken"}}>
         <button className="close-button btn btn-sm btn-error btn-square bg-primary right-0 top-0 mt-1 mr-1 btn-outline absolute transition-all duration-150 hover:scale-110" onClick={(e, isShown) => {props.func(e, true);}}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
         </button>
@@ -13,7 +13,7 @@ const CatCardClicked = (props) => {
           <div className="font-bold text-lg pt-4" style={{fontSize: "clamp(1rem, 2vw, 2rem)"}}>{"Weight (kg): " + ((props.cat?.breeds[0]?.weight.metric) || "No weight information")}</div>
         </>}
           <div className="text-justify font-bold mt-4 text-lg" style={{fontSize: "clamp(1rem, 1.5vw, 2rem)"}}>{props.cat?.breeds[0]?.description || "Either a friendly cat or a total demon."}</div>
-          <div className="button wrapper flex flex-col justify-center align-center items-center">
+          <div className="button self-end wrapper flex flex-col justify-center align-center items-center">
             <button className="btn align-end p-2 h-full transition-all duration-200 hover:scale-110 bg-dark border-none rounded-lg mt-4" style={{fontSize: "clamp(1rem, 1.5vw, 3rem)"}} onClick={props.addToFavFunc}>❤️ ADD TO FAVOURITES</button>
             <a className="btn align-end p-2 h-full transition-all duration-200 hover:scale-110 bg-dark border-none rounded-lg mt-4" href={props.cat.url} target="_blank" style={{fontSize: "clamp(1rem, 1.5vw, 3rem)"}}>See picture in full screen</a>
           </div>
