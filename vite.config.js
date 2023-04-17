902db80c0961509a5d134344f18557e53ada30a3
bttobi <bttobi123@gmail.com>
2023-04-17 15:52:32 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/cat_page',
-  plugins: [react()]
+  plugins: [react()],
+  fallback: '/404.html'
 })
