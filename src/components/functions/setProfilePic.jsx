cf59b1c9e531352c5991f3298b2dcc5f02438504
bttobi <bttobi123@gmail.com>
2023-04-19 15:34:11 +0200

    profile picture setting and getting

---
+++ src/components/functions/setProfilePic.jsx
@@ -0,0 +1,10 @@
+import { collection, doc, setDoc } from 'firebase/firestore';
+import db from '../../firebase';
+
+const setProfilePic = async (auth, url) => {
+  const collectionRef = collection(db, auth.currentUser.email);
+  const documentRef = doc(collectionRef,  "profile-picture");
+  await setDoc(documentRef, {url: `${url}`});
+}
+
+export default setProfilePic
