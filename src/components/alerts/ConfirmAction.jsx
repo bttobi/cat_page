1037fcdb1c097ca052ae4cc7e280925d69b5656a
bttobi <bttobi123@gmail.com>
2023-04-21 10:30:35 +0200

    Update ConfirmAction.jsx

--- src/components/alerts/ConfirmAction.jsx
+++ src/components/alerts/ConfirmAction.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const ConfirmAction = (props) => {
-  return (<>
-      <input type="checkbox" id="my-modal-4" className="modal-toggle" checked={props.show}/>
+  return (<> //props.show
+      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
       <label htmlFor="my-modal-4" className="modal cursor-pointer">
         <label className="modal-box relative" htmlFor="">
           <h3 className="text-lg font-bold">Are you sure to delete your account?</h3>
