86bebe9fcf4d68dcfe33d839ea9477fe48d87570
bttobi <bttobi123@gmail.com>
2023-04-21 10:29:40 +0200

    added component to confirm actions - yet to be styled

---
+++ src/components/alerts/ConfirmAction.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ConfirmAction = (props) => {
+  return (<>
+      <input type="checkbox" id="my-modal-4" className="modal-toggle" checked={props.show}/>
+      <label htmlFor="my-modal-4" className="modal cursor-pointer">
+        <label className="modal-box relative" htmlFor="">
+          <h3 className="text-lg font-bold">Are you sure to delete your account?</h3>
+          <button className="btn py-4">DELETE ACCOUNT</button>
+        </label>
+      </label>
+    </>
+  )
+}
+
+export default ConfirmAction
