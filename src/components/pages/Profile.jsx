52cf111340b300a3f7daf461251ff19ac8a3fb13
bttobi <bttobi123@gmail.com>
2023-04-21 10:10:47 +0200

    added sign out notification

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,18 +1,41 @@
 import { useState, useContext, useEffect, useRef } from 'react';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { UserContext } from '../../App';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import useProfilePic from '../hooks/useProfilePic';
+import SuccessNotification from '../alerts/SuccessNotification';
+import FailedNotification from '../alerts/FailedNotification';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const Profile = () => {
   const [user, setUser] = useState({});
-  const auth = useContext(UserContext);
   const [profilePicUrl, isFetched] = useProfilePic();
+  const [notificationMessage, setNotificationMessage] = useState("");
+  const [isNotificationShown, setIsNotificationShown] = useState(false);
+  const [errorHappened, setErrorHappened] = useState(false);
+  const navigate = useNavigate();
+  const auth = useContext(UserContext);
   const newPasswordRef = useRef();
 
-  const logOut = async () => {
-    await signOut(auth);
+  const logOut = () => {
+    signOut(auth)
+    .then(()=> {
+      setNotificationMessage("Signed out successfully!");
+      setIsNotificationShown(true);
+      setTimeout(()=>{
+        setIsNotificationShown(false);
+        navigate("/");
+      }, 2000)
+    })
+    .catch(error => {
+      setNotificationMessage("Some errors happened!");
+      setErrorHappened(true);
+      setTimeout(()=>{
+        setErrorHappened(false);
+        navigate("/");
+      }, 2000)
+    })
   };
 
   const deleteAccount = () => {
@@ -27,20 +50,22 @@ const Profile = () => {
   }
   
   useEffect(() => {
-    setUser(auth?.currentUser);
+    onAuthStateChanged(auth, (currentUser) =>{
+      setUser(currentUser);
+    });
     window.scrollTo(0, 0);
   }, []);
 
   return (<>
-    {(user?.email != undefined || user?.email != null) ? 
+    {(user?.email != undefined || user?.email != null || isNotificationShown || errorHappened) ? 
     <motion.div initial={{scaleY: 0}} animate={{scaleY: 1}} exit={{scaleY: 0}} className="font-article text-white w-auto h-auto mt-32 flex flex-col text-lg font-bold justify-center align-center items-center rounded-lg bg-dark p-4 shadow-lg shadow-black">
-      <p>Welcome! </p>
-      <p>{user?.email}</p>
+      <p>Welcome!</p>
+      <p>{user?.email != undefined || user?.email != null ? user?.email : "Signed out!"}</p>
       {profilePicUrl != "NOT FOUND" ? 
       <div className="w-36 h-36 rounded-lg mt-4 border-white border-2" style={{backgroundImage: `url(${profilePicUrl})`, backgroundSize: "cover", backgroundPosition: "center"}} />
       : <div className="w-36 h-36 rounded-lg border-white border-2 p-3 flex justify-center align-center items-center text-center mt-4">No profile picture found!</div>}
-      <button className="btn btn-sm bg-primary text-article text-secondary-white text-xl border-2 border-secondary-white rounded-md transition-all duration-300 hover:border-secondary-white hover:bg-dark mt-4" onClick={logOut}><Link to="/">Sign out</Link></button>
-      {user?.email != "test@account.com" &&
+      <button className="btn btn-sm bg-primary text-article text-secondary-white text-xl border-2 border-secondary-white rounded-md transition-all duration-300 hover:border-secondary-white hover:bg-dark mt-4" onClick={logOut}>Sign out</button>
+      {user?.email != "test@account.com" && !isNotificationShown && !errorHappened &&
       <>
         <button className="btn btn-sm bg-primary text-article text-secondary-white text-xl border-2 border-secondary-white rounded-md transition-all duration-300 hover:border-secondary-white hover:bg-dark mt-4" onClick={deleteAccount}>Delete Account</button>
         <div className="btn btn-sm bg-primary text-article text-secondary-white text-xl border-2 border-secondary-white rounded-md transition-all duration-300 hover:border-secondary-white hover:bg-dark mt-4" onClick={()=>{}}>Change Password</div>
@@ -53,6 +78,10 @@ const Profile = () => {
       <Link className="underline" to="/">Log in here</Link>
     </motion.div>
     }
+    <AnimatePresence>
+      {isNotificationShown && <SuccessNotification notification={ notificationMessage }/>}
+      {errorHappened && <FailedNotification notification={ notificationMessage }/>}
+    </AnimatePresence>
     </>
   )
 }
