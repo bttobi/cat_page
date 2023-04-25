bb3c95036447e86c1934b4182c1c0fd2719f989f
bttobi <bttobi123@gmail.com>
2023-04-25 12:34:14 +0200

    added function to reset password and to delete an account

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useState, useContext, useEffect, useRef } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { UserContext } from '../../App';
 import { Link, useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
+import { signOut, sendPasswordResetEmail } from 'firebase/auth';
 import useProfilePic from '../hooks/useProfilePic';
 import SuccessNotification from '../alerts/SuccessNotification';
 import FailedNotification from '../alerts/FailedNotification';
@@ -30,7 +30,7 @@ const Profile = () => {
       setIsNotificationShown(true);
       setTimeout(()=>{
         setIsNotificationShown(false);
-        navigate("/");
+        navigate("/login");
       }, 2000)
     })
     .catch(error => {
@@ -38,17 +38,53 @@ const Profile = () => {
       setErrorHappened(true);
       setTimeout(()=>{
         setErrorHappened(false);
-        navigate("/");
       }, 2000)
     })
   };
 
   const deleteAccount = () => {
-    setShowDeleteAccount(true);
-  // TO DO - delete all favs and profile picture too
+    user?.delete()
+    .then(()=> {
+      setShowDeleteAccount(true);
+      setNotificationMessage("Deleted user successfully!");
+      setNotificationIcon(<TailSpin stroke={"#000"}/>);
+      setIsNotificationShown(true);
+      setTimeout(()=>{
+        setIsNotificationShown(false);
+        navigate("/login");
+      }, 2000)
+    })
+  
+    .catch(error => {
+      setNotificationMessage("Some errors happened!");
+      setErrorHappened(true);
+      setTimeout(()=>{
+        setErrorHappened(false);
+      }, 2000)
+    });
+
+    //TO DO need to remove all documents from user
+    //TO DO ADD MODAL TO BE SURE
   };
 
   const changePassword = () => {
+    sendPasswordResetEmail(auth, user?.email)
+    .then(() => {
+      setNotificationMessage("Email with password reset instructions sent!");
+      setNotificationIcon(null);
+      setIsNotificationShown(true);
+
+      setTimeout(()=>{
+        setIsNotificationShown(false);
+      }, 2000);
+    })
+    .catch((error) => {
+      setNotificationMessage("Some errors happened!");
+      setErrorHappened(true);
+      setTimeout(()=>{
+        setErrorHappened(false);
+      }, 2000)
+    });
   };
 
   const changeEmail = async () => {
@@ -71,12 +107,12 @@ const Profile = () => {
       <div className="w-36 h-36 rounded-lg mt-4 border-white border-2" style={{backgroundImage: `url(${profilePicUrl})`, backgroundSize: "cover", backgroundPosition: "center"}} />
       : <div className="w-36 h-36 rounded-lg border-white border-2 p-3 flex justify-center align-center items-center text-center mt-4">No profile picture found!</div>}
       <button className="btn btn-sm bg-primary text-article text-secondary-white text-xl border-2 border-secondary-white rounded-md transition-all duration-300 hover:border-secondary-white hover:bg-dark mt-4" onClick={logOut}>Sign out</button>
-      {/*user?.email != "test@account.com" && !isNotificationShown && !errorHappened &&
+      {user?.email != "test@account.com" &&
       <>
         <button className="btn btn-sm bg-primary text-article text-secondary-white text-xl border-2 border-secondary-white rounded-md transition-all duration-300 hover:border-secondary-white hover:bg-dark mt-4" onClick={deleteAccount}>Delete Account</button>
-        <div className="btn btn-sm bg-primary text-article text-secondary-white text-xl border-2 border-secondary-white rounded-md transition-all duration-300 hover:border-secondary-white hover:bg-dark mt-4" onClick={()=>{}}>Change Password</div>
+        <div className="btn btn-sm bg-primary text-article text-secondary-white text-xl border-2 border-secondary-white rounded-md transition-all duration-300 hover:border-secondary-white hover:bg-dark mt-4" onClick={changePassword}>Change Password</div>
       </>
-    */}
+      }
     </motion.div>  : 
     <motion.div initial={{scaleY: 0}} animate={{scaleY: 1}} exit={{scaleY: 0}} className="font-article text-white mt-32 flex flex-col text-lg font-bold justify-center align-center items-center rounded-lg bg-dark p-4 shadow-lg shadow-black">
       <span>You need to log in to view the profile! </span>
