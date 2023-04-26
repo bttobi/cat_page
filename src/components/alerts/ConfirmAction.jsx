0110ed45c77c75a75411d656dc7624b8621a14cb
bttobi <bttobi123@gmail.com>
2023-04-26 15:19:53 +0200

    added modal to delete account

--- src/components/alerts/ConfirmAction.jsx
+++ src/components/alerts/ConfirmAction.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import SuccessNotification from '../alerts/SuccessNotification';
+import FailedNotification from '../alerts/FailedNotification';
+import { AnimatePresence } from 'framer-motion';
+import TailSpin from 'react-loading-icons/dist/esm/components/tail-spin';
+import { useNavigate } from 'react-router-dom';
 
-const ConfirmAction = (props) => {
+const ConfirmAction = ({ userToDelete }) => {
+  const [notificationMessage, setNotificationMessage] = useState("");
+  const [isNotificationShown, setIsNotificationShown] = useState(false);
+  const [errorHappened, setErrorHappened] = useState(false);
+  const [notificationIcon, setNotificationIcon] = useState(null);
+  const navigate = useNavigate();
+
+  const deleteAccount = () => {
+    userToDelete?.delete()
+    .then(()=> {
+      setNotificationMessage("Deleted user successfully!");
+      setNotificationIcon(<TailSpin stroke={"#000"}/>);
+      setIsNotificationShown(true);
+      setTimeout(()=>{
+        setIsNotificationShown(false);
+        navigate("/login");
+      }, 2000)
+    })
+  
+    .catch(error => {
+      console.log(error)
+      setNotificationMessage("Some errors happened!");
+      setErrorHappened(true);
+      setTimeout(()=>{
+        setErrorHappened(false);
+      }, 2000)
+    });
+
+    //TO DO need to remove all documents from user
+    //TO DO ADD MODAL TO BE SURE
+  };
+
   return (<>
-      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-      <label htmlFor="my-modal-4" className="modal cursor-pointer">
-        <label className="modal-box relative" htmlFor="">
-          <h3 className="text-lg font-bold">Are you sure to delete your account?</h3>
-          <button className="btn py-4">DELETE ACCOUNT</button>
+      <input type="checkbox" id="del-acc" className="modal-toggle" />
+      <label htmlFor="del-acc" className="modal cursor-pointer font-article text-white">
+        <label className="modal-box relative bg-dark flex flex-col justify-center align-center shadow-md shadow-black" htmlFor="">
+          <label htmlFor="del-acc" className="close-button btn btn-error btn-square bg-primary w-12 right-0 top-0 mt-2 mr-2 btn-outline fixed transition-all duration-150 hover:scale-110">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+          </label>
+          <div className="wrapper flex flex-col justify-center align-center items-center">
+            <h3 className="text-3xl font-bold mt-8 text-center">Are you sure to delete your account?</h3>
+            <button className="btn w-min text-center mt-8 text-white bg-red-500 hover:bg-red-700 border-0" onClick={deleteAccount}>DELETE ACCOUNT</button>
+          </div>
         </label>
+      <AnimatePresence>
+        {isNotificationShown && <SuccessNotification notification={ notificationMessage } icon={ notificationIcon }/>}
+        {errorHappened && <FailedNotification notification={ notificationMessage }/>}
+      </AnimatePresence>
       </label>
     </>
   )
