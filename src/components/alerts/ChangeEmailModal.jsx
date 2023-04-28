97bf4f9c1b9c8b51bdd12fda342fc5cabce44454
bttobi <bttobi123@gmail.com>
2023-04-28 16:06:12 +0200

    created ChangeEmailModal functionality with the database moving function

---
+++ src/components/alerts/ChangeEmailModal.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useRef, useContext } from 'react';
+import SuccessNotification from './SuccessNotification';
+import FailedNotification from './FailedNotification';
+import { AnimatePresence } from 'framer-motion';
+import { updateEmail } from 'firebase/auth';
+import { UserContext } from '../../App';
+import { collection, doc, getDoc, setDoc, deleteDoc } from 'firebase/firestore';
+import db from '../../firebase';
+
+export const ChangeEmailModal = ({userToChange}) => {
+  const [notificationMessage, setNotificationMessage] = useState("");
+  const [isNotificationShown, setIsNotificationShown] = useState(false);
+  const [errorHappened, setErrorHappened] = useState(false);
+  const [notificationIcon, setNotificationIcon] = useState(null);
+  const [email, setEmail] = useState("");
+  const emailRef = useRef(null);
+  const auth = useContext(UserContext);
+
+  const resetPassword = async () => {
+    //TO DO copy current user collection with new email, remove the old one
+
+    //get the current data
+    const collectionRef = collection(db, auth.currentUser.email);
+    const favDocumentRef = doc(collectionRef, "favourites");
+    const profDocumentRef = doc(collectionRef, "profile-picture");
+
+    const favDocumentSnap = await getDoc(favDocumentRef);
+    const profDocumentSnap = await getDoc(profDocumentRef);
+
+
+    //TO DO BETTER ERROR HANDLING
+    await updateEmail(userToChange, email)
+    .then(async () => {
+      console.log(auth.currentUser.email);
+      //setting the new data
+      const newCollectionRef = collection(db, auth.currentUser.email);
+      const newFavDocumentRef = doc(newCollectionRef, "favourites");
+      const newProfDocumentRef = doc(newCollectionRef, "profile-picture");
+      console.log(favDocumentSnap.data())
+      await setDoc(newFavDocumentRef, favDocumentSnap.data());
+      await setDoc(newProfDocumentRef, profDocumentSnap.data());
+
+
+      //deleting the old data
+      const favDocumentRef = doc(collectionRef, "favourites");
+      const profDocumentRef = doc(collectionRef, "profile-picture");
+      await deleteDoc(favDocumentRef);
+      await deleteDoc(profDocumentRef);
+
+
+      setNotificationMessage("Email updated!");
+      setNotificationIcon(null);
+      setIsNotificationShown(true);
+
+      setTimeout(()=>{
+        setIsNotificationShown(false);
+        window.location.reload();
+      }, 2000);
+    })
+    .catch((error) => {
+      console.log(error)
+      setNotificationMessage("Provide a valid email!");
+      setErrorHappened(true);
+      setTimeout(()=>{
+        setErrorHappened(false);
+      }, 2000)
+    });
+  };
+
+  return (
+    <>
+      <input type="checkbox" id="change-email" className="modal-toggle" />
+      <label htmlFor="change-email" className="modal cursor-pointer font-article text-white">
+        <label className="modal-box relative bg-dark flex flex-col justify-center align-center shadow-md shadow-black" htmlFor="">
+          <label htmlFor="change-email" className="close-button btn btn-error btn-square bg-primary w-12 right-0 top-0 mt-2 mr-2 btn-outline fixed transition-all duration-150 hover:scale-110">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+          </label>
+          <div className="wrapper flex flex-col justify-center align-center items-center">
+            <p className="text-3xl font-bold mt-8 text-center">Do you want to change your email?</p>
+            <input className="input-email input w-full max-w-xs mt-8" ref={emailRef} type="email" name="email" required placeholder='Email' onChange={()=>{setEmail(emailRef.current.value)}}/>
+            <button className="btn h-min text-center mt-8 text-white bg-green-500 hover:bg-green-700 border-0" onClick={resetPassword}>CHANGE EMAIL</button>
+          </div>
+        </label>
+      <AnimatePresence>
+        {isNotificationShown && <SuccessNotification notification={ notificationMessage } icon={ notificationIcon }/>}
+        {errorHappened && <FailedNotification notification={ notificationMessage }/>}
+      </AnimatePresence>
+      </label>
+    </>
+  )
+}
+
+export default ChangeEmailModal
