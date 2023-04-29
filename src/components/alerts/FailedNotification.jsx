338305f5d267fc0593b829831bb0d7ae596e4b3b
bttobi <bttobi123@gmail.com>
2023-04-29 16:05:26 +0200

    added custom icon

--- src/components/alerts/FailedNotification.jsx
+++ src/components/alerts/FailedNotification.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
-const FailedNotification = ({ notification }) => {
+const FailedNotification = ({ notification, icon }) => {
   return (
     <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="fixed top-20 w-auto alert alert-warning shadow-md shadow-black z-30 flex flex-row justify-center align-center items-center content-center">
-      <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+      {icon != undefined && icon != null ? icon : 
+      <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>}
       <p style={{margin: "0"}}>{ notification }</p>
     </motion.div>
   )
