d94a4f687656a903d6338338dbd8f1bd2f71a9bc
bttobi <bttobi123@gmail.com>
2023-05-01 15:30:59 +0200

    added error handling when Cat Breeds fetching fails

--- src/components/pages/CatBreeds.jsx
+++ src/components/pages/CatBreeds.jsx
@@ -4,11 +4,12 @@ import CatCard from '../assets/CatCard';
 import LoadingIcons from 'react-loading-icons';
 import Search from '../interactive-elements/Search';
 import useBreeds from '../hooks/useBreeds';
+import FailedNotification from '../alerts/FailedNotification';
 
 const CatBreeds = () => {
   const [catBreedId, setCatBreedId] = useState("beng");
   const [showClicked, setShowClicked] = useState(false);
-  const [cats, isFetching] = useBreeds(catBreedId);
+  const [cats, isFetching, isError] = useBreeds(catBreedId);
 
   const getSearchDetailsOfCats = (searchDetails) => {
     setCatBreedId(searchDetails.breed);
@@ -22,7 +23,7 @@ const CatBreeds = () => {
     window.scrollTo(0, 0);
   }, []);
 
-  return (
+  return (<>
       <motion.div id="cat-breeds" initial={{scaleY: 0}} animate={{scaleY: 1}} exit={{scaleY: 0}} className="home-page w-full m-none my-16 flex flex-col justify-start items-center font-article text-white">
         <AnimatePresence>
           {showClicked && <motion.div initial={{opacity: 0}} animate={{opacity: 0.7}} exit={{opacity: 0}} className="backdrop w-full h-full fixed flex flex-col bg-black z-10 align-center justify-center items-center"></motion.div>}
@@ -37,6 +38,10 @@ const CatBreeds = () => {
             cats.map(el =>  <CatCard showClicked={getShowClicked} isFavourite={ false } cat={el} key={el.url}/>)}
         </div>
       </motion.div>
+      <AnimatePresence>
+        {isError &&<FailedNotification notification={"Fetching cat breeds failed!"}/>}
+      </AnimatePresence>
+      </>
   )
 }
 
