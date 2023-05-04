8f1f23acb1fe0ac4bff93860a0f458ee45808764
bttobi <bttobi123@gmail.com>
2023-05-04 19:21:41 +0200

    tweaks with removing profile picture

--- src/components/alerts/ProfilePictureModal.jsx
+++ src/components/alerts/ProfilePictureModal.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { AnimatePresence } from "framer-motion";
 import { collection, doc, deleteDoc } from 'firebase/firestore';
 import db from '../../firebase';
 import Notification from './Notification';
+import { ProfilePictureContext } from '../../App';
 
-const ProfilePictureModal = ({profilePicture, userToChange}) => {
+const ProfilePictureModal = ({userToChange}) => {
   const [showNotification, setShowNotification] = useState(false);
   const [notificationMessage, setNotificationMessage] = useState("");
   const [errorHappened, setErrorHappened] = useState(false);
+  const [userPicture, setUserPicture] = useContext(ProfilePictureContext);
 
   const removeProfilePicture = async () => {
     try{
@@ -16,14 +18,15 @@ const ProfilePictureModal = ({profilePicture, userToChange}) => {
       await deleteDoc(documentRef);
       setShowNotification(true);
       setErrorHappened(false);
+      setUserPicture("NOT FOUND");
       setNotificationMessage("Profile picture removed successfully!");
-      setTimeout(()=>{setShowNotification(false); window.location.reload();}, 2000);
+      setTimeout(()=>{setShowNotification(false);}, 2000)
     }
     catch(error){
       setShowNotification(true);
       setErrorHappened(true);
       setNotificationMessage("Errors happened while removing profile picture!");
-      setTimeout(()=>{setShowNotification(false)}, 2000);
+      setTimeout(()=>{setShowNotification(false);}, 2000)
     }
   }
 
@@ -37,9 +40,13 @@ const ProfilePictureModal = ({profilePicture, userToChange}) => {
           </label>
           <div className="wrapper flex flex-col justify-center align-center items-center">
             <p className="text-3xl font-bold mt-8 text-center">What do you want to do with the profile picture?</p>
-            <div className="w-48 h-48 rounded-lg mt-4 border-white border-2" style={{backgroundImage: `url(${profilePicture})`, backgroundSize: "cover", backgroundPosition: "center"}} />
-            <button className="btn h-min text-center mt-8 text-white bg-red-500 hover:bg-red-700 border-0" onClick={removeProfilePicture}>REMOVE PROFILE PIC</button>
-            <a className="btn h-min text-center mt-8 text-white bg-green-500 hover:bg-green-700 border-0" href={profilePicture} target="_blank">SEE PROFILE PIC IN FULLSCREEN</a>
+            {userPicture != "NOT FOUND" ? 
+            <>
+              <div className="w-48 h-48 rounded-lg mt-4 border-white border-2" style={{backgroundImage: `url(${userPicture})`, backgroundSize: "cover", backgroundPosition: "center"}} />
+              <button className="btn h-min text-center mt-8 text-white bg-red-500 hover:bg-red-700 border-0" onClick={removeProfilePicture}>REMOVE PROFILE PIC</button>
+              <a className="btn h-min text-center mt-8 text-white bg-green-500 hover:bg-green-700 border-0" href={userPicture} target="_blank">SEE PROFILE PIC IN FULLSCREEN</a>
+            </>: 
+            <div className="w-48 h-48 rounded-lg text-lg border-white border-2 p-3 flex justify-center align-center items-center text-center mt-4">No profile picture found!</div>}
           </div>
         </label>
       <AnimatePresence>
