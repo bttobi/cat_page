056978885238fb4b0fb79601a6b643f3eb6aade8
bttobi <bttobi123@gmail.com>
2023-05-04 19:21:26 +0200

    added profile picture context for responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 import { auth } from './firebase.js';
 import { motion, AnimatePresence } from 'framer-motion';
 import Navbar from './components/navigation/Navbar';
@@ -14,9 +14,11 @@ import Profile from './components/pages/Profile';
 import Random from './components/pages/Random';
 
 export const UserContext = createContext();
+export const ProfilePictureContext = createContext([null, ()=>{}]);
 
 function App() {
   const queryClient = new QueryClient();
+  const [userPicture, setUserPicture] = useState(null);
 
   return (
     <UserContext.Provider value={auth}>
@@ -24,19 +26,21 @@ function App() {
         <QueryClientProvider client={queryClient}>
           <AnimatePresence mode="wait">
             <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="App w-full h-full flex flex-col items-center relative">
-              <Navbar />
-                <AnimatePresence mode="wait">
-                  <Routes>
-                    <Route path='/' element={<Random/>}/>
-                    <Route path='/cat_breeds' element={<CatBreeds/>}/>
-                    <Route path='/favourites' element={<Favourites/>}/>
-                    <Route path='/about' element={<About/>}/>
-                    <Route path='/login' element={<Login/>}/>
-                    <Route path='/register' element={<Register/>}/>
-                    <Route path='/profile' element={<Profile/>}/>
-                    <Route path={"*"} element={<Random />}/>
-                  </Routes>
-              </AnimatePresence>
+              <ProfilePictureContext.Provider value={[userPicture, setUserPicture]}>
+                <Navbar />
+                  <AnimatePresence mode="wait">
+                    <Routes>
+                      <Route path='/' element={<Random/>}/>
+                      <Route path='/cat_breeds' element={<CatBreeds/>}/>
+                      <Route path='/favourites' element={<Favourites/>}/>
+                      <Route path='/about' element={<About/>}/>
+                      <Route path='/login' element={<Login/>}/>
+                      <Route path='/register' element={<Register/>}/>
+                      <Route path='/profile' element={<Profile/>}/>
+                      <Route path={"*"} element={<Random />}/>
+                    </Routes>
+                </AnimatePresence>
+              </ProfilePictureContext.Provider>
             </motion.div>
           </AnimatePresence>
         </QueryClientProvider>
