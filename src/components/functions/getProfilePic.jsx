d93106a5810412dc9c44613fddcbc78cc86da5fd
bttobi <bttobi123@gmail.com>
2023-05-04 19:22:39 +0200

    made useProfilePic into function to also set profile picture context

--- src/components/functions/getProfilePic.jsx
+++ src/components/functions/getProfilePic.jsx
@@ -2,32 +2,31 @@ import { useState, useEffect, useContext } from 'react';
 import db from '../../firebase';
 import { collection, doc, getDoc } from 'firebase/firestore';
 import { UserContext } from '../../App';
+import { ProfilePictureContext } from '../../App';
 
-const useProfilePic = () => {
+const getProfilePic = () => {
   const auth = useContext(UserContext);
+  const [userPicture, setUserPicture] = useContext(ProfilePictureContext);
   const [isFetched, setIsFetched] = useState(false);
-  const [profilePic, setProfilePic] = useState("");
 
   const getProfilePicture = async () => {
-    if(auth.currentUser?.email==null || auth.currentUser?.email==undefined)
-    return;
-
+    if(auth?.currentUser?.email==null || auth?.currentUser?.email==undefined) return;
     
     try{
       const collectionRef =  collection(db, auth.currentUser.email);
       const documentRef = doc(collectionRef, "profile-picture");
       const profilePictureDoc = await getDoc(documentRef);
       if(profilePictureDoc.data().url == undefined || profilePictureDoc.data().url == null){
-        setProfilePic("NOT FOUND");
+        setUserPicture("NOT FOUND");
         setIsFetched(true);
         return;
       }
-      setProfilePic(profilePictureDoc.data().url);
+      setUserPicture(profilePictureDoc.data().url);
       setIsFetched(true);
     }
 
     catch(error){
-      setProfilePic("NOT FOUND");
+      setUserPicture("NOT FOUND");
       setIsFetched(true);
     }
   }
@@ -40,7 +39,7 @@ const useProfilePic = () => {
     getProfilePicture();
   }, []);
 
-  return [profilePic, isFetched];
+  return [userPicture, isFetched];
 }
 
-export default useProfilePic
+export default getProfilePic
