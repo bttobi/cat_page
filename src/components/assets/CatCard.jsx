d3e1b195e6d0928b24c3d5355772c11edac4e991
bttobi <bttobi123@gmail.com>
2023-05-06 16:02:23 +0200

    showing custom names, added ability to change names

--- src/components/assets/CatCard.jsx
+++ src/components/assets/CatCard.jsx
@@ -5,10 +5,14 @@ import { motion, AnimatePresence } from 'framer-motion';
 import addToFav from '../functions/addToFav';
 import removeFromFav from '../functions/removeFromFav';
 import Notification from '../alerts/Notification';
+import changeName from '../functions/changeName';
 
 const CatCard = (props) => {
   const catDetails = useRef();
+  const inputChangeNameRef = useRef();
+  const editNameRef = useRef();
   const [isShown, setIsShown] = useState(false);
+  const [showEditName, setShowEditName] = useState(false);
   const [errorHappened, setErrorHappened] = useState(false);
   const [isFavourite, setIsFavourite] = useState(props?.isFavourite | false);
   const [showNotification, setShowNotification] = useState(false);
@@ -49,6 +53,44 @@ const CatCard = (props) => {
     }
   }
 
+  const editName = async (customName) => {
+    try {
+      if(customName.trim() == "") throw "white_char";
+      else if(customName.length > 12) throw "too_long";
+
+      changeName(props, auth, customName);
+      setErrorHappened(false);
+      props.cat.customName = customName;
+      setNotificationMessage("Success - changed name!");
+      setShowNotification(true);
+      setTimeout(()=> {
+        setShowNotification(false);
+      }, 2000)
+    }
+    catch(error){
+      setErrorHappened(true);
+      let dispError;
+      switch (error){
+        case "white_char":
+          dispError = "No white characters!"
+          break;
+
+        case "too_long":
+          dispError = "Maximum 12 characters!"
+          break;
+        
+        default:
+          dispError = "Errors happened while changing name!";
+          break;
+      }
+      setNotificationMessage(dispError);
+      setShowNotification(true);
+      setTimeout(()=> {
+        setShowNotification(false);
+      }, 2000)
+    }
+  }
+
   const showDetails = () => {
     setIsShown(true);
     props.showClicked(true);
@@ -58,6 +100,11 @@ const CatCard = (props) => {
     if(catDetails.current != null && (!catDetails.current.contains(e.target) || clicked)){
       setIsShown(false);
       props.showClicked(false);
+    }
+
+    if(inputChangeNameRef.current != null && (!inputChangeNameRef.current.contains(e.target) || clicked)
+    && !editNameRef.current.contains(e.target) || clicked){
+      setShowEditName(false);
     }
   }
 
@@ -72,21 +119,27 @@ const CatCard = (props) => {
     <>
       <AnimatePresence>
         <motion.div initial={{transform: 'scale(0)'}} whileHover={{transform: 'scale(1.05)'}} animate={{transform: 'scale(1)'}} exit={{transform: 'scale(0)'}} className="cat-wrapper w-min h-min mx-4 mt-8 flex flex-col bg-primary rounded-lg shadow-lg shadow-black">
-        <div className="favourite py-6 pl-6 pr-2 w-full h-8 flex flex-row justify-end items-center">
+          <div className="favourite py-6 pr-2 pl-2 w-full h-8 flex flex-row justify-center items-center align-center">
+            <button className="fav-button absolute ml-2 left-0 align-start w-min h-min transition-all duration-200 hover:scale-125" onClick={()=>{setShowEditName(!showEditName)}}>{isFavourite ? (showEditName ? <span ref={editNameRef} onClick={()=>{editName(inputChangeNameRef?.current?.value)}}>✅</span> : "✏️") : ""}</button>
             <div className="description-wrapper w-full h-20 flex justify-center border-secondary-white rounded-lg">
-              <p className="description w-full h-full flex flex-wrap justify-center items-center font-article font-bold text-center text-xl">{props.cat?.breeds[0]?.name || "Cute Cat"}</p>
+              <p className="description w-full h-full flex flex-wrap justify-center items-center font-article font-bold text-center text-xl">
+                <AnimatePresence>
+                  {showEditName && isFavourite ? <motion.input ref={inputChangeNameRef} initial={{scaleX: 0}} animate={{scaleX: 1}} exit={{scaleX: 0}} className="input input-sm w-28" placeholder="Edit name" type="text"/> : 
+                  <motion.span initial={{scaleX: 0}} animate={{scaleX: 1}}>{props.cat?.customName ? props.cat?.customName : (props.cat?.breeds[0]?.name ?? "Cute Cat")}</motion.span>}
+                </AnimatePresence>
+              </p>
             </div>
-            <button className="fav-button align-end w-min h-min transition-all duration-200 hover:scale-125" onClick={handleFavourites}>{isFavourite ? "🗑️" : "❤️"}</button>
+            <button className="fav-button absolute right-0 mr-2 align-end w-min h-min transition-all duration-200 hover:scale-125" onClick={handleFavourites}>{isFavourite ? "🗑️" : "❤️"}</button>
           </div>
-            <div className="flex flex-col justify-center items-center cursor-pointer" onClick={showDetails}>
-              <div className="w-min h-min m-0 rounded-lg" style= {{minWidth: '12rem', minHeight: '12rem', backgroundImage: `url(${props.cat.url})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}} alt="cat"></div>           
-            </div>
+          <div className="flex flex-col justify-center items-center cursor-pointer" onClick={showDetails}>
+            <div className="w-min h-min m-0 rounded-lg" style= {{minWidth: '12rem', minHeight: '12rem', backgroundImage: `url(${props.cat.url})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}} alt="cat"></div>           
+          </div>
         </motion.div>
       </AnimatePresence>
       <AnimatePresence>
       {isShown &&
         <motion.div initial={{y: '-10rem', opacity: 0}} animate={{y: '0', opacity: 1}} exit={{opacity: 0}} className="fixed top-28 flex flex-col justify-center align-center items-center z-10 filter-blur-0" ref={catDetails}>
-            <CatCardClicked cat={props.cat} isFavourite={isFavourite} showFunc={hideDetails} handleFavFunc={handleFavourites}/>
+            <CatCardClicked cat={props.cat} isFavourite={isFavourite} showFunc={hideDetails} handleFavFunc={handleFavourites} handleEditNameFunc={editName}/>
         </motion.div>}
       </AnimatePresence>
       {/* DISPLAY NOTIFICATIONS */}
--- src/components/assets/CatCardClicked.jsx
+++ src/components/assets/CatCardClicked.jsx
@@ -1,14 +1,16 @@
 import setProfilePic from "../functions/setProfilePic";
-import { useState, useContext } from "react";
+import { useState, useContext, useRef } from "react";
 import { UserContext } from '../../App';
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Notification from "../alerts/Notification";
 import { ProfilePictureContext } from '../../App';
 
 const CatCardClicked = (props) => {
   const auth = useContext(UserContext);
+  const inputChangeNameRef = useRef();
   const [errorHappened, setErrorHappened] = useState(false);
   const [showNotification, setShowNotification] = useState(false);
+  const [showEditName, setShowEditName] = useState(false);
   const [notificationMessage, setNotificationMessage] = useState("");
   const [, setUserPicture] = useContext(ProfilePictureContext);
 
@@ -44,17 +46,20 @@ const CatCardClicked = (props) => {
         <button className="close-button w-12 btn btn-error btn-square bg-primary right-0 top-0 mt-1 mr-1 btn-outline fixed transition-all duration-150 hover:scale-110" onClick={(e, isShown) => {props.showFunc(e, true);}}>
           <svg xmlns="http://www.w3.org/2000/svg" className="w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
         </button>
-        <div className="cat-name-details text-center border-secondary-white text-4xl font-bold" style={{fontSize: "clamp(2rem, 5vw, 4rem)"}}>{props.cat?.breeds[0]?.name || "Cute Cat"}</div>
+        {showEditName ? 
+          <motion.input initial={{scaleX: 0}} animate={{scaleX: 1}} exit={{scaleX: 0}} ref={inputChangeNameRef} className="input input-lg" placeholder="Edit name"/> 
+          : <motion.div initial={{scaleX: 0}} animate={{scaleX: 1}} className="cat-name-details text-center border-secondary-white text-4xl font-bold" style={{fontSize: "clamp(2rem, 5vw, 4rem)"}}>{props.cat.customName ?? props.cat?.breeds[0]?.name ?? "Cute Cat"}</motion.div>}
         <div className="cat-description-wrapper mt-2 lg:mt-8">
         {(props.cat.breeds[0] != null || props.cat.breeds[0] != undefined) && 
         <div className="w-full flex flex-col justify-center items-center">
-          <div className="font-bold text-lg" style={{fontSize: "clamp(1rem, 3vw, 2rem)"}}>{"Origin: " + (props.cat?.breeds[0]?.origin || "no origin information")}</div>
-          <div className="font-bold text-lg pt-4" style={{fontSize: "clamp(1rem, 3vw, 2rem)"}}>{"Weight (kg): " + ((props.cat?.breeds[0]?.weight.metric) || "No weight information")}</div>
+          <div className="font-bold text-lg" style={{fontSize: "clamp(1rem, 3vw, 2rem)"}}>{"Origin: " + (props.cat?.breeds[0]?.origin ?? "no origin information")}</div>
+          <div className="font-bold text-lg pt-4" style={{fontSize: "clamp(1rem, 3vw, 2rem)"}}>{"Weight (kg): " + ((props.cat?.breeds[0]?.weight.metric) ?? "No weight information")}</div>
         </div>}
-          <div className="text-justify font-bold text-lg lg:px-8 lg:mt-8 mt-2 px-2 leading-normal" style={{fontSize: "clamp(0.8rem, 1.5vw, 1.5rem)"}}>{props.cat?.breeds[0]?.description || "Either a friendly cat or a total demon."}</div>
+          <div className="text-justify font-bold text-lg lg:px-8 lg:mt-8 mt-2 px-2 leading-normal" style={{fontSize: "clamp(0.8rem, 1.5vw, 1.5rem)"}}>{props.cat?.breeds[0]?.description ?? "Either a friendly cat or a total demon."}</div>
         </div>
         <div className="button-wrapper lg:mt-8 mt-0 self-end h-full flex flex-col justify-center align-end w-full items-center content-center">
             <button className="btn align-end p-2 h-full transition-all duration-200 hover:scale-110 bg-dark border-none rounded-lg mt-4" style={{fontSize: "clamp(0.8rem, 1.5vw, 1rem)"}} onClick={props.handleFavFunc}>{props.isFavourite ? "🗑️ REMOVE FROM FAVOURITES" : "❤️ ADD TO FAVOURITES"}</button>
+            {props.isFavourite ? <button className="btn align-end p-2 h-full transition-all duration-200 hover:scale-110 bg-dark border-none rounded-lg mt-4" style={{fontSize: "clamp(0.8rem, 1.5vw, 1rem)"}} onClick={()=>{setShowEditName(!showEditName)}}>{showEditName ? <span onClick={()=>{props.handleEditNameFunc(inputChangeNameRef?.current?.value)}}>SET NEW NAME</span> : "EDIT NAME"}</button> : <></>}
             <a className="btn align-end p-2 h-full transition-all duration-200 hover:scale-110 bg-dark border-none rounded-lg mt-4" href={props.cat.url} target="_blank" style={{fontSize: "clamp(0.8rem, 1.5vw, 1rem)"}}>See picture in full screen</a>
             <button className="btn align-end p-2 h-full transition-all duration-200 hover:scale-110 bg-dark border-none rounded-lg mt-4" style={{fontSize: "clamp(0.8rem, 1.5vw, 1rem)"}} onClick={handleProfilePic}>SET AS PROFILE PICTURE</button>
         </div>
