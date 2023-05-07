cc8899f3a2c4c170e284ba210baf48eea88dfc1d
bttobi <bttobi123@gmail.com>
2023-05-07 13:35:08 +0200

    changed fetching times

--- src/components/hooks/useFavourites.jsx
+++ src/components/hooks/useFavourites.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect, useContext } from 'react';
-import db from '../../firebase';
-import { collection, doc, getDoc } from 'firebase/firestore';
-import { UserContext } from '../../App';
+import { useState, useEffect, useContext } from "react";
+import db from "../../firebase";
+import { collection, doc, getDoc } from "firebase/firestore";
+import { UserContext } from "../../App";
 
 const useFavourites = () => {
   const auth = useContext(UserContext);
@@ -10,43 +10,42 @@ const useFavourites = () => {
   const [error, setError] = useState(false);
 
   const getFavCats = async () => {
-    if(auth.currentUser?.email==null || auth.currentUser?.email==undefined){
+    if (auth.currentUser?.email == null || auth.currentUser?.email == undefined)
       return;
-    }
-    
-    try{
-      const collectionRef =  collection(db, auth.currentUser.email);
+
+    try {
+      const collectionRef = collection(db, auth.currentUser.email);
       const documentRef = doc(collectionRef, "favourites");
       const listOfCats = await getDoc(documentRef);
-      
-      if(!listOfCats.exists() || listOfCats.data().favourites == []){
+
+      if (!listOfCats.exists() || listOfCats.data().favourites == []) {
         setCats([]);
         return;
       }
 
       const toAdd = [];
-      listOfCats.data().favourites.forEach(favCat => toAdd.push(favCat));
+      listOfCats.data().favourites.forEach((favCat) => toAdd.push(favCat));
       setCats(toAdd);
       setIsFetched(true);
-    }
-
-    catch(err){
+    } catch (err) {
       setError(true);
       return;
     }
-  }
+  };
 
-  if(!isFetched){ //fetch until loaded
-    setTimeout(()=>{
+  if (!isFetched) {
+    //fetch until loaded
+    const time = error ? 5000 : 500;
+    setTimeout(() => {
       getFavCats();
-    }, 2000)
+    }, time);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getFavCats();
   }, []);
 
   return [cats, isFetched, error];
-}
+};
 
 export default useFavourites;
