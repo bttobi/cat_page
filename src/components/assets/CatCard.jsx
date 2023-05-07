cda6e27ce6d7496366a8a32894ad073ce48d4711
bttobi <bttobi123@gmail.com>
2023-05-07 14:43:20 +0200

    break word fix

--- src/components/assets/CatCard.jsx
+++ src/components/assets/CatCard.jsx
@@ -128,7 +128,7 @@ const CatCard = (props) => {
         <motion.div initial={{transform: 'scale(0)'}} whileHover={{transform: 'scale(1.05)'}} animate={{transform: 'scale(1)'}} exit={{transform: 'scale(0)'}} className="cat-wrapper w-min h-min mx-4 mt-8 flex flex-col bg-primary rounded-lg shadow-lg shadow-black">
           <div className="favourite py-6 pr-2 pl-2 w-full h-14 flex flex-row justify-center items-center align-center">
             <button className="fav-button absolute ml-2 left-0 align-start w-min h-min transition-all duration-200 hover:scale-125" onClick={()=>{setShowEditName(!showEditName)}}>{isFavourite ? (showEditName ? <span ref={editNameRef} onClick={()=>{editName(inputChangeNameRef?.current?.value)}}>✅</span> : "✏️") : ""}</button>
-            <div className="description-wrapper w-min h-20 flex justify-center border-secondary-white rounded-lg">
+            <div className="description-wrapper w-32 break-words h-20 flex justify-center border-secondary-white rounded-lg">
               <p className="description w-full h-full flex flex-wrap justify-center items-center font-article font-bold text-center text-xl">
                 <AnimatePresence>
                   {showEditName && isFavourite ? <motion.input ref={inputChangeNameRef} onKeyDown={handleKeys} initial={{scaleX: 0}} animate={{scaleX: 1}} exit={{scaleX: 0}} className="input input-sm w-28" placeholder="Edit name" type="text"/> : 
