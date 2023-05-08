23bc01c6df0cddf96b4f36ae5327d81efff86b0f
bttobi <bttobi123@gmail.com>
2023-05-08 13:24:17 +0200

    code cleanup

--- src/components/functions/changeName.jsx
+++ src/components/functions/changeName.jsx
@@ -9,10 +9,6 @@ const changeName = async (props, auth, customName) => {
       const catToEdit = documentSnap.data().favourites
         .find((cat)=>cat.url == props.cat.url);
 
-      const index = documentSnap.data().favourites
-        .findIndex((cat)=>cat.url == props.cat.url);
-        //TO DO - ADD CATS TO APPROPRIATE PLACE - IF POSSIBLE
-
       await updateDoc(documentRef, {favourites: arrayRemove(catToEdit)})
     
       catToEdit.customName = customName;
